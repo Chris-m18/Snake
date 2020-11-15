6877a1ce7bfbb327f00c7f6454f1cfffd389f283
Christian Matos <chris@Christians-MBP.fios-router.home>
2020-11-15 14:44:58 -0500

    almost there

--- snake.js
+++ snake.js
@@ -1,57 +1,92 @@
 let canvas, ctx;
 
-window.onload = function () {
-  canvas =
-    document.getElementById("canvas");
+window.onload = function() {
+  canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
 
-  document.addEventListener("keydown", keydownEvent);
+  document.addEventListener("keydown", keyDownEvent);
 
+  // render X times per second
   let x = 8;
-  setInterval(drew, 1000 / x);
-
+  setInterval(draw, 1000 / x);
 };
 
-//gameworld
-let gridSize = tileSize = 20;  // 20 * 20 = 400
-let nextX = nextY = 0;
+// game world
+let gridSize = (tileSize = 20); // 20 x 20 = 400
+let nextX = (nextY = 0);
 
-//apple
+// snake
+let defaultTailSize = 3;
+let tailSize = defaultTailSize;
+let snakeTrail = [];
+let snakeX = (snakeY = 10);
+
+// apple
 let appleX = (appleY = 15);
 
-//Move snake
-snakeX += nextX;
-snakeY += nextY;
+// draw
+function draw() {
+  // move snake in next pos
+  snakeX += nextX;
+  snakeY += nextY;
 
-//snkae over game world 
-if (snakeX < 0) {
-  snakeX = gridSize - 1;
+  // snake over game world?
+  if (snakeX < 0) {
+    snakeX = gridSize - 1;
+  }
+  if (snakeX > gridSize - 1) {
+    snakeX = 0;
+  }
+
+  if (snakeY < 0) {
+    snakeY = gridSize - 1;
+  }
+  if (snakeY > gridSize - 1) {
+    snakeY = 0;
+  }
+
+  //snake bite apple?
+  if (snakeX == appleX && snakeY == appleY) {
+    tailSize++;
+
+    appleX = Math.floor(Math.random() * gridSize);
+    appleY = Math.floor(Math.random() * gridSize);
+  }
+
+  //paint background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // paint snake
+  ctx.fillStyle = "green";
+  for (let i = 0; i < snakeTrail.length; i++) {
+    ctx.fillRect(
+      snakeTrail[i].x * tileSize,
+      snakeTrail[i].y * tileSize,
+      tileSize,
+      tileSize
+    );
+
+    //snake bites it's tail?
+    if (snakeTrail[i].x == snakeX && snakeTrail[i].y == snakeY) {
+      tailSize = defaultTailSize;
+    }
+  }
+
+  // paint apple
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleX * tileSize, appleY * tileSize, tileSize, tileSize);
+
+  //set snake trail
+  snakeTrail.push({ x: snakeX, y: snakeY });
+  while (snakeTrail.length > tailSize) {
+    snakeTrail.shift();
+  }
 }
-if (snakeX > gridSize - 1) {
-  snakeX = 0;
-}
-if (snakeY < 0) {
-  snakeY = gridSize - 1;
-}
-if (snakeY > gridSize - 1) {
-  snakeY = 0;
-}
 
-//snakebit 
-
-
-
-
-
-//snake
-let defaultTailSize = 3;
-let tailSize = defaultTailSize
-let snakeTrail = [];
-let snakeX = snakeY = 10;
-
-
+// input
 function keyDownEvent(e) {
-  switch (e.KeyCode) {
+  switch (e.keyCode) {
     case 37:
       nextX = -1;
       nextY = 0;
@@ -69,6 +104,4 @@ function keyDownEvent(e) {
       nextY = 1;
       break;
   }
-};
-
-
+}
